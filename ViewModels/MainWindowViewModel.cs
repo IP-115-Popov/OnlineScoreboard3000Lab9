830cbe68b8de3d31ee9419807ba7637dcc1ceca5
IP-115-Popov <99480504+IP-115-Popov@users.noreply.github.com>
2023-05-07 19:03:28 +0700

    Fick Buton TimeRase

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,3 +1,5 @@
+using Avalonia.Animation;
+using ReactiveUI;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -6,6 +8,55 @@ namespace OnlineScoreboard3000.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => "Welcome to Avalonia!";
+        private bool checkYesterday;
+        private bool checkToday;
+        private bool checkTomorrow;
+
+        public MainWindowViewModel()
+        {
+            CheckYesterday = false;
+            CheckToday = true;
+            CheckTomorrow = false;
+        }
+        public bool CheckYesterday
+        {
+            get => checkYesterday;
+            set
+            {
+                this.RaiseAndSetIfChanged(ref checkYesterday, value);
+                if (CheckYesterday == true)
+                {
+                    CheckToday = false;
+                    CheckTomorrow = false;
+                }
+            }
+        }
+        public bool CheckToday
+        {
+            get => checkToday;
+            set 
+            {
+                this.RaiseAndSetIfChanged(ref checkToday, value);
+                if (CheckToday == true)
+                {
+                    CheckYesterday = false;
+                    CheckTomorrow = false;
+                }
+            }
+        }
+        public bool CheckTomorrow
+        {
+            get => checkTomorrow;
+            set
+            {
+                this.RaiseAndSetIfChanged(ref checkTomorrow, value);
+                if (CheckTomorrow == true)
+                {
+                    CheckYesterday = false;
+                    CheckToday = false;
+                }
+            }
+        }
+
     }
 }

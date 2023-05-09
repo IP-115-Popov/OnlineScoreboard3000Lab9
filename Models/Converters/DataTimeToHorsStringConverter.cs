1c56f793ecab0eebfee275cad763b9bc03fe7407
IP-115-Popov <99480504+IP-115-Popov@users.noreply.github.com>
2023-05-09 18:20:36 +0700

    DataTimeToHorsStringConverter

---
+++ Models/Converters/DataTimeToHorsStringConverter.cs
@@ -0,0 +1,26 @@
+﻿using Avalonia;
+using Avalonia.Data.Converters;
+using Avalonia.Platform;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineScoreboard3000.Models.Converters
+{
+    public class DataTimeToHorsStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((DateTime)value).Hour.ToString() + ":" + ((DateTime)value).Minute.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
